89cc9c62bca8ace2d15ee3509a5895a9da1e9ac9
Daria Nikolaeva <nikolaevdaaa@gmail.com>
2025-04-30 13:52:27 +0300

    username

--- app/app.py
+++ app/app.py
@@ -47,7 +47,7 @@ def index():
     user = session.get("user")
     if user:
         return render_template_string('''
-            <h1>Welcome, {{ user['name'] }}!</h1>
+            <h1>Welcome, {{ user['preferred_username'] }}!</h1>
             <form action="{{ url_for('logout') }}" method="post">
                 <button type="submit">Logout</button>
             </form>
